b9b577f2575f9103d778123bd43846d22147cb66
adrianoyuji <adrianoyuji@gmail.com>
2021-02-07 22:36:20 -0400

    hosted resume on google

--- components/ui/ResumeButton/index.tsx
+++ components/ui/ResumeButton/index.tsx
@@ -1,7 +1,7 @@
 const ResumeButton = () => {
   return (
     <a
-      href="/api/resume"
+      href="https://docs.google.com/document/d/e/2PACX-1vTKxSpvOTelyFQTdmJ09wa8Zoh9Mz5CIU9s0EqErv7kIdZszF3rNVOxdJtuqs0d42UpZWsyDPN75NMO/pub"
       target="_blank"
       className="text-center mt-8 py-3 px-6 bg-white hover:bg-gray-100 active:bg-gray-300 rounded-lg text-black font-bold w-full sm:w-64 shadow-lg"
     >
